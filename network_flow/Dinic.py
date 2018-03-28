64d7167f4322bd4ec4ae48dc6143769479b0b815
Ashee <tim956956a@gmail.com>
2018-03-28 18:11:55 +0800

    Dinic OK

--- network_flow/Dinic.py
+++ network_flow/Dinic.py
@@ -1,17 +1,37 @@
 from copy import deepcopy
 
-def read_graph() :
+def read_graph(path) :
+    # #
+    # G = {0:{1:10,2:10},
+    #      1:{2:2,3:4,4:8},
+    #      2:{4:9},
+    #      3:{5:10},
+    #      4:{3:6,5:10},
+    #      5:{}}
+    #
+    # return G,len(G)
+    data = open(path,"r")
+    lines = data.read().splitlines()
+    n = int(lines[0])
+    m = int(lines[1])
+    G = {i:{} for i in range(n)}
 
-    G = {0:{1:10,2:10},
-         1:{2:2,3:4,4:8},
-         2:{4:9},
-         3:{5:10},
-         4:{3:6,5:10},
-         5:{}}
+    for line in lines[2:] :
+        arc = line.split("  ")
 
-    return G,len(G),9
+        arc[2] = "{:.0f}".format(float(arc[2]))
+        v = int(arc[0])
+        u = int(arc[1])
+        capacity = int(arc[2])
+        # print(v,u,capacity)
+        try :
+            G[u][v]
+        except KeyError :
+            G[v][u] = capacity
+    # print(G)
+    return G,n
 
-def residule_graph(G,G_f) :
+def residual_graph(G,G_f) :
     residule_G = deepcopy(G)
     for i in range(len(G)) :
         for j in G[i] :
@@ -22,13 +42,13 @@ def residule_graph(G,G_f) :
 
 def get_level_graph (G,G_f) :
     #using BFS to get level graph
-    G_r = residule_graph(G,G_f)
-
+    G_r = residual_graph(G,G_f)
+    n = len(G)
     visited = [False]*n
     level = [0] * n
-    level_graph = {key : {} for key in range(n)}
+    level_graph = {}
     queue = [0]
-
+    connect = [[] for _ in range(n)]
     while queue :
         v = queue.pop(0) #deque
         for i in G_r[v] :
@@ -37,59 +57,90 @@ def get_level_graph (G,G_f) :
                 queue.append(index)
                 visited[index] = True
                 level[index] = level[v] + 1
+        if v == n - 1:
+            try :
+                del level_graph[level[v]+1]
+            except KeyError :
+                pass
+            break
         for i in G_r[v]:
             index, flow = i, G_r[v][i]
             if level[index] > level[v] :
-                level_graph[v][index] = flow
-    # print(level_graph)
-    return level_graph
+                try :
+                    level_graph[level[v]+1][index] = flow
+                except KeyError :
+                    level_graph[level[v]+1] = {index:flow}
+                connect[v].append(index)
 
-def find_path(level_graph, source) :
-    v = source #start from source
+    return level_graph,connect
+
+def find_path(level_graph ,connect, source=0) :
+
+    path = []
+
+    stack = [(source,[source])]
+    visited = set()
+    while stack :
+        (vertex,path) = stack.pop()
+        if vertex not in visited :
+            if vertex == n-1 :
+                return path
+            for i in connect[vertex] :
+                stack.append((i,path+[i]))
+    return path
+def find_min_flow(path) :
     min_flow = float('inf') #big number
-    path = [source]
-    while v != n-1 and level_graph[v]:
-        # print(level_graph.keys(level_graph.values()),)
-        # print(level_graph.items())
-        # print(list(level_graph.keys())[0])
-        next_v,flow = list(level_graph[v].keys())[0],list(level_graph[v].values())[0]
-        path.append(next_v)
-        v = next_v
-        if flow < min_flow :
-            min_flow = flow
+    if path[len(path) - 1] == n - 1  :
+        for i in range(len(path) - 1):
+            u = path[i]
+            v = path[i + 1]
+            flow = G[u][v] - G_f[u][v]
+            if flow < min_flow :
+                min_flow = flow
+        return  min_flow
+    else :
+        return False
+def update_graph(level_graph, G_f, aug_path, min_flow, connect):
+    for i in range(1,len(aug_path)):
+        v = aug_path[i-1]
+        j = aug_path[i]
 
-    return path,min_flow if v == n-1 else False
-def update_graph(level_graph,G_f,aug_path,min_flow) :
-    for i in range(len(aug_path)-1) :
-        v = aug_path[i]
-        j = aug_path[i+1]
-
-        level_graph[v][j] -= min_flow
         G_f[v][j] += min_flow
-        if level_graph[v][j] == 0 :
-            del level_graph[v][j]
+        if G_f[v][j] ==  G[v][j]:
+            # del level_graph[i][j]
+            connect[v].remove(j)
 
     return level_graph
 
 
-
-if __name__ == "__main__" :
-    G,n,m = read_graph() #graph, # of vertices , # of edges
+if __name__ == "__main__":
+    # read_graph("./Data/elist1440.txt")
+    G, n = read_graph("./Data/karzanov_example2.txt")  # graph, # of vertices , # of edges
     G_f = deepcopy(G)
 
-    for i in range(len(G_f)) :
+    for i in range(len(G_f)):
         for j in G_f[i]:
             G_f[i][j] = 0
 
     total_flow = 0
-    for _ in range(m) :
-        level_graph= get_level_graph(G,G_f)
+    prev_flow = 0
+    while 1 :
+        level_graph,connect= get_level_graph(G,G_f)
+        for i in level_graph :
+            print(i,level_graph[i])
+        prev_flow = total_flow
         while 1 :
-            aug_path,min_flow = find_path(level_graph,0)
+            aug_path = find_path(level_graph,connect)
+            min_flow = find_min_flow(aug_path)
+            print(aug_path,min_flow)
             total_flow += min_flow
-            level_graph = update_graph(level_graph,G_f,aug_path,min_flow)
             if min_flow == False :  #blocking flow
                 break
+            level_graph = update_graph(level_graph,G_f,aug_path,min_flow,connect)
+
+        if prev_flow == total_flow :
+            break
+
     print("Max Flow : ",total_flow)
 
 
--- network_flow/Karzanov.py
+++ network_flow/Karzanov.py
@@ -1,3 +1,139 @@
-from Dinic import read_graph
+from copy import deepcopy
+from Dinic import get_level_graph
+
+global balanced
+max_num = 1000000
+def read_graph(path) :
+
+    data = open(path, "r")
+    lines = data.read().splitlines()
+    n = int(lines[0])
+
+    G = {i: {} for i in range(n)}
+    G_b = {i:{} for i in range(n)} #graph for backward
+
+    for line in lines[2:]:
+        arc = line.split("  ")
+
+        arc[2] = "{:.0f}".format(float(arc[2]))
+        v = int(arc[0])
+        u = int(arc[1])
+        capacity = int(arc[2])
+        # print(v,u,capacity)
+        try:
+            G[u][v]
+        except KeyError:
+            G[v][u] = capacity
+            G_b[u][v] = capacity
+    # print(G)
+    return G,G_b, n
+
+def residual_graph(G,G_f) :
+    residual_G = deepcopy(G)
+    for i in range(len(G)) :
+        for j in G[i] :
+            residual_G[i][j] -= G_f[i][j]
+    # print(residual_G)
+    return residual_G
+def init_G_f(G) :
+    G_f = deepcopy(G)
+    for i in range(len(G_f)) :
+        G_f[i]['inflow'] = 0
+        G_f[i]['outflow'] = 0
+        for j in G_f[i]:
+            G_f[i][j] = 0
+    G_f[0]['inflow'] = max_num
+    G_f[len(G_f)-1]['outflow'] = max_num
+    return G_f
+def top_order(G) :
+    stack = []
+    top = {}
+    counter = 1
+    count = [0 for i in range(len(G))]
+
+    for i in G :
+        for j in G[i] :
+            count[j] += 1
+    for i in range(len(G)):
+        if count[i] == 0:
+            stack.append(i)
+
+    top[0] = [0]
+    for _ in range(len(G)) :
+        changed = False
+        top[counter] = []
+        if not stack :
+            return "error"
+        else :
+            v = stack.pop()
+            for i in G[v] :
+                count[i] -= 1
+                if count[i] == 0 :
+                    changed = True
+                    stack.append(i)
+                    top[counter].append(i)
+        if changed :
+            counter += 1
+    del top[counter] #delete last one
+
+    return top
+def forward_step(G,G_f,top,blocked):
+    for layer in top :
+        for v in top[layer]:
+            for node in G[v]:
+                if G_f[v]['inflow'] > G_f[v]['outflow'] and balanced[node] == False :
+                    flow = min(G_f[v]['inflow'] - G_f[v]['outflow'], G[v][node])
+                    G_f[v]['outflow'] += flow-G_f[v][node]
+                    G_f[node]['inflow'] += flow-G_f[v][node]
+                    G_f[v][node] = flow
+                elif blocked[v] != 0 : #balance blocked node
+                    for node in G[v] :
+                        flow = min(G_f[v]['outflow'] - G_f[v]['inflow'],G[v][node] )
+                        G_f[v][node] -= flow
+                        G_f[v]['outflow'] -= flow
+                        G_f[node]['inflow'] -= flow
+                        if G_f[v]['inflow'] == G_f[v]['outflow'] :
+                            balanced[v] = True
+                            break
+            if(G_f[v]['inflow'] > G_f[v]['outflow'] and v != 0 and v !=  len(G)-1) :
+                blocked[v] = 1 #PF_blocked
+    return G_f
+
+def backward_step(G_b,G_f,top,blocked):
+    for layer in range(len(top)-1,0,-1) :
+        for v in top[layer] :
+            for node in G_b[v]:
+                if G_f[v]['inflow'] < G_f[v]['outflow'] and balanced[node] == False:
+                        flow = min(G_f[v]['outflow']-G_f[v]['inflow'] , G_b[v][node])
+                        G_f[node]['outflow'] += flow-G_f[node][v]
+                        G_f[v]['inflow'] += flow-G_f[node][v]
+                        G_f[node][v] = flow
+                elif blocked[v] != 0 : #balance blocked node
+                    for node in G_b[v] :
+                        flow = min(G_f[v]['inflow'] - G_f[v]['outflow'],G_b[v][node] )
+                        G_f[node][v] -= flow
+                        G_f[v]['inflow'] -= flow
+                        G_f[node]['outflow'] -= flow
+                        if G_f[v]['inflow'] == G_f[v]['outflow'] :
+                            balanced[v] = True
+                            break
+            if (G_f[v]['inflow'] < G_f[v]['outflow'] and v != 0 and v != len(G) - 1):
+                blocked[v] = -1  # BF_blocked
+    return G_f
 if __name__ == "__main__" :
-    G,n,m = read_graph() #graph, # of vertices , # of edges
+    G,G_b,n = read_graph("./Data/test1.txt") #graph, # of vertices , # of edges
+
+    G_f = init_G_f(G)
+    top = top_order(G)
+    blocked = [0 for i in range(n)] #0 for unblocked, 1 for PF blocked ,-1 for BF blocked
+    balanced = [False] * n
+
+    for _ in range(10):
+    # while any(i == False for i in balanced[1:n-1]):
+        G_f = forward_step(G,G_f,top,blocked)
+        G_f = backward_step(G_b,G_f,top,blocked)
+
+
+    print("Max Flow :",G_f[0]['outflow'])
+
+
